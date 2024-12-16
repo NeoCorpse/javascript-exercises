a96950ac11f5d4726de46c0244ec766a98decbec
Ian <neocorpse@proton.me>
2024-12-16 20:21:26 +0300

    Complete exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+	let str = '';
+	if (num < 0) return 'ERROR';
+	for (let i = 1; i <= num; i++) {
+		str += string;
+	}
+	return str;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
