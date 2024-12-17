77b4f9178db5e7f4d545348bf79e6075eb18fc09
Ian <neocorpse@proton.me>
2024-12-17 17:19:21 +0300

    Complete exercise 03
    I created an infinite for loop somehow :( and blamed my computer for not running the loop.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let str = string.split('')
+    let array = []
+    for (let i = str.length -1; i >= 0 ; i--) {
+        array.push(str[i])
+    }
+    array = array.join('')
+    return array
 };
-
 // Do not edit below this line
 module.exports = reverseString;
