5e2745a15eac154f8eca1f26b1a43b324699bf8e
Ian <neocorpse@proton.me>
2024-12-18 20:30:29 +0300

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+	//(x - 32) * 5/4
+	let t = +(((temp - 32) * 5) / 9).toFixed(1);
+	if (t === t.toFixed(0)) return Math.round(t);
+	else return t;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+	//x °C ≘ (x * 9/5 + 32)
+	let t = +((temp * 9) / 5 + 32).toFixed(1);
+	if (t === t.toFixed(0)) return t.round(t);
+	else return t;
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+	convertToCelsius,
+	convertToFahrenheit,
 };
