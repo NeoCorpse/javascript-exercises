7632b3809c9d72238b31484e6ff095aa93e2594e
Ian <neocorpse@proton.me>
2024-12-18 13:23:46 +0300

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+	if (a <0 || b <0 || a !== Math.floor(a) || b !== Math.floor(b)) {
+		return 'ERROR';
+	}
+	let sum = 0;
+	if (a > b) {
+		let temp = a;
+		a = b;
+		b = temp;
+	}
+	for (let i = a; i <= b; i++) {
+		sum += i;
+	}
+	return sum;
 };
 
 // Do not edit below this line
