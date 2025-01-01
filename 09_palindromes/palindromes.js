89769f35d32a2342322bac398e8d725d1bdb046f
Ian <neocorpse@proton.me>
2025-01-01 18:43:35 +0300

    Complete exercises indicated in objects basics lesson

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+	string = string.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '').toLowerCase();
+	reverse = string.split('').reverse().join('');
+	return string === reverse;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  return people.reduce((former, current) => {
+    let formerAge =
+      "yearOfDeath" in former
+        ? former.yearOfDeath - former.yearOfBirth
+        : new Date().getFullYear() - former.yearOfBirth;
+    let currentAge =
+      "yearOfDeath" in current
+        ? current.yearOfDeath - current.yearOfBirth
+        : new Date().getFullYear() - current.yearOfBirth;
+    return currentAge > formerAge ? current : former;
+  });
 };
 
 // Do not edit below this line
